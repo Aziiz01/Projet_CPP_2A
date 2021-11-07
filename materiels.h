e02df788a233a9c5605760572f5605fc7618480b
rimeh <rimeh.mechichi@esprit.tn>
2021-11-07 12:11:53 +0100

    first commit main.cpp

---
+++ materiels.h
@@ -0,0 +1,30 @@
+#ifndef materiels_H
+#define materiels_H
+#include <QString>
+#include <QSqlQueryModel>
+
+class Materiels
+{
+public:
+    Materiels();
+    Materiels(QString,QString,int,int);
+    int getcode_mat();
+    int getquantite_mat();
+    QString getintitule_mat();
+    QString getfournisseur();
+    void setcode_mat(int);
+    void setquantite_mat(int);
+    void setintitule_mat(QString);
+    void setfournisseur(QString);
+    bool ajouter();
+    QSqlQueryModel* afficher();
+    bool supprimer(int);
+    bool modifier(int code_mat);
+private:
+    QString intitule_mat,fournisseur;
+    int code_mat;
+    int quantite_mat;
+
+};
+
+#endif // materiels_H
